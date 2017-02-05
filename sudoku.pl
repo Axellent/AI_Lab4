4da13a904087124d845908f4d641d12b3f2a255d
limeemil <limeemil@users.noreply.github.com>
2017-02-05 21:42:16 +0100

    Added: base for solving sudokus.

--- sudoku.pl
+++ sudoku.pl
@@ -1,1 +1,27 @@
 :- use_module(library(clpfd)).
+
+/* Takes in a sudoku problem as a list of lists and solves it */
+solve(Rows) :- 
+	length(Rows, 9), maplist(same_length(Rows), Rows),
+	append(Rows, Values), Values ins 1..9,
+	maplist(all_distinct, Rows),
+	transpose(Rows, Columns),
+	maplist(all_distinct, Columns),
+	Rows = [As, Bs, Cs, Ds, Es, Fs, Gs, Hs, Is],
+	blocks(As, Bs, Cs), blocks(Ds, Es, Fs), blocks(Gs, Hs, Is).
+	
+/* True if there is exactly one of each number in every block */	
+blocks([], [], []).
+blocks([N1, N2, N3 | Ns1], [N4, N5, N6 | Ns2], [N7, N8, N9 | Ns3]) :-
+		all_distinct([N1, N2, N3, N4, N5, N6, N7, N8, N9]),
+		blocks(Ns1, Ns2, Ns3).
+		
+problem(1, [[_, _, _, _, 7, _, _, 6, _],
+			[_, _, _, _, _, _, _, _, _],
+			[2, _, _, _, _, _, _, _, _],
+			[_, _, _, _, _, _, _, _, _],
+			[_, 9, _, _, 5, _, _, _, _],
+			[_, _, _, _, _, _, 1, _, _],
+			[_, _, 4, _, _, _, _, _, _],
+			[_, _, _, _, _, _, _, _, _],
+			[_, _, _, _, _, 3, 8, _, _]]).
